3a1a268edc346820f1e8db8cd8429bd1ed90a9a9
JN <john.nat.hill@gmail.com>
2016-12-22 13:05:32 +0100

    On modifie les donnes envoyes

--- generate.py
+++ generate.py
@@ -20,7 +20,7 @@ def topologie(src,dst):
 			nodes.append(topo[1])
 			dst.write("set n%s [$ns node]\n" %(topo[1]))
 
-		dst.write("$ns duplex-link $n%s $n%s %sMb %sms DropTail\n" %(topo[0], topo[1],topo[2],topo[3]))
+		dst.write("$ns duplex-link $n%s $n%s %sGb %sms DropTail\n" %(topo[0], topo[1],topo[2],topo[3]))
 		dst.write("$ns queue-limit $n%s $n%s 10\n" %(topo[0],topo[1]))
 		dst.write("\n")
 
