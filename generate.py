9d8f95f10b621b60671f5d9c0d04839653cc3f49
JN <john.nat.hill@gmail.com>
2016-12-22 11:45:56 +0100

    Essai avec pleins de petits flux

--- generate.py
+++ generate.py
@@ -29,13 +29,14 @@ def trafic( src, dst, sim_time, burst, idle, shape):
 
 	debut = math.floor(sim_time * 0.10)
 	fin = sim_time - debut
+	conv_g_k = 1000*1000
 
 	for line in src:
 		
 		traf = line.rstrip('\n\r').split(" ")
 
 		pareto_traf = math.floor(0.85 * float(traf[2]))
-		ftp_traf = ( int(traf[2]) - pareto_traf ) * 1024*1024*8
+		ftp_traf = ( int(traf[2]) - pareto_traf ) * conv_g_k
 
 		dst.write("set sink_%s_%s [new Agent/TCPSink]\n" %(traf[0], traf[1]))
 		dst.write("$ns attach-agent $n%s $sink_%s_%s\n" %(traf[1], traf[0], traf[1]))
@@ -45,28 +46,37 @@ def trafic( src, dst, sim_time, burst, idle, shape):
 		dst.write("$ns connect $tcp_%s_%s $sink_%s_%s\n" %(traf[0], traf[1], traf[0], traf[1]))
 
 		dst.write("set p_%s_%s [new Application/Traffic/Pareto]\n" %(traf[0], traf[1]))
-		rate = pareto_traf * 1024*1024*8 / sim_time
+		rate = pareto_traf * conv_g_k * 1000 / sim_time
 		dst.write("$p_%s_%s set burst_time_ %s\n" %(traf[0], traf[1], burst))
 		dst.write("$p_%s_%s set idle_time_ %s\n" %(traf[0], traf[1], idle))
 		dst.write("$p_%s_%s set rate_ %s\n" %(traf[0], traf[1], rate))
 		dst.write("$p_%s_%s set packetSize_ 1500\n" %(traf[0], traf[1]))
 		dst.write("$p_%s_%s set shape_ %s\n" %(traf[0], traf[1], shape))
 		dst.write("$p_%s_%s attach-agent $tcp_%s_%s\n" %(traf[0], traf[1], traf[0], traf[1]))
+
 		dst.write("$ns at %s \"$p_%s_%s start\"\n\n" %(debut, traf[0], traf[1]))
 		dst.write("$ns at %s \"$p_%s_%s stop\"\n\n" %(fin, traf[0], traf[1]))
 
 		random_traf = 0
 		i = 0
-		offset = math.floor(ftp_traf * 0.0001)
+		offset = math.floor(ftp_traf * 0.00001)
 
 		while random_traf < ftp_traf:
+
 			zipf = np.random.zipf(shape)
 			instant = rand.random() * (fin - debut) + debut
+
+			dst.write("set tcp_%s_%s_%s [new Agent/TCP]\n" %(traf[0], traf[1], i))
+			dst.write("$tcp_%s_%s_%s set packetSize_ 1500\n" %(traf[0], traf[1], i))
+			dst.write("$ns attach_agent $n%s $tcp_%s_%s_%s " %(traf[0], traf[0], traf[1], i))
+			dst.write("$ns connect $tcp_%s_%s_%s $sink_%s_%s\n" %(traf[0], traf[1], i, traf[0], traf[1]))
+
 			dst.write("set ftp_%s_%s_%s [new Application/FTP]\n" %(traf[0], traf[1], i))
-			dst.write("$ftp_%s_%s_%s attach-agent $tcp_%s_%s\n" %(traf[0], traf[1], i, traf[0], traf[1]))
+			dst.write("$ftp_%s_%s_%s attach-agent $tcp_%s_%s_%s\n" %(traf[0], traf[1], i, traf[0], traf[1], i))
 			dst.write("$ftp_%s_%s_%s set type_ FTP\n" %(traf[0], traf[1], i))
-			dst.write("$ns at %s \"$ftp_%s_%s_%s send %s\"\n" %(instant, traf[0], traf[1], i, zipf*1500 + offset))
-			random_traf += zipf*1500 + offset
+			dst.write("$ns at %s \"$ftp_%s_%s_%s send %s Kb\"\n" %(instant, traf[0], traf[1], i, zipf + offset))
+
+			random_traf += zipf + offset
 			i+=1
 
 
@@ -80,7 +90,7 @@ src_traf = open("traff.traf","r")
 dest  = open("simulation.tcl","w")
 
 dest.write("set ns [new Simulator]\n")
-dest.write("set f [open out.tr w]\n$ns namtrace-all $f\n")
+dest.write("set f [open out.tr w]\n$ns trace-all $f\n")
 dest.write("proc finish {} {\n")
 dest.write("    global ns f\n")
 dest.write("    $ns flush-trace\n")
