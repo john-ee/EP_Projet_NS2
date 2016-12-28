ab89f2ce91f26a406d454d582689c7d99f189fc4
JN <john.nat.hill@gmail.com>
2016-12-28 11:38:39 +0100

    Compteur pertes -> compteurs proportions

--- loss_count.py
+++ loss_count.py
@@ -5,14 +5,18 @@ import numpy as np
 src = open("out.tr","r")
 
 pertes = []
+recus = []
 for i in range(25):
 	pertes.append([i] * 25)
+	recus.append([i] * 25)
 
 for line in src:
 	event = line.rstrip('\n\r').split(" ")
 
 	if event[0] == 'd':
 		pertes[int(event[2])][int(event[3])]+=1
+	elif event[0] == 'r':
+		recus[int(event[2])][int(event[3])]+=1
 
 imax = [0]*3
 jmax = [0]*3
@@ -21,15 +25,15 @@ valmax = [0]*3
 for i in range(len(pertes)):
 	for j in range(len(pertes[i])):
 		if pertes[i][j] > valmax[0]:
-			valmax[0] = pertes[i][j]
+			valmax[0] = pertes[i][j] / recus[i][j]
 			imax[0] = i
 			jmax[0] = j
 		elif pertes[i][j] > valmax[1]:
-			valmax[1] = pertes[i][j]
+			valmax[1] = pertes[i][j] / recus[i][j]
 			imax[1] = i
 			jmax[1] = j
 		elif pertes[i][j] > valmax[2]:
-			valmax[2] = pertes[i][j]
+			valmax[2] = pertes[i][j] / recus[i][j]
 			imax[2] = i
 			jmax[2] = j
 
@@ -37,9 +41,9 @@ for i in range(len(pertes)):
 dst  = open("info.txt", "a")
 
 dst.write("Les liens les plus faibles sont:\n")
-dst.write("\t%s-%s avec %s pertes\n" %(imax[0], jmax[0], valmax[0]))
-dst.write("\t%s-%s avec %s pertes\n" %(imax[1], jmax[1], valmax[1]))
-dst.write("\t%s-%s avec %s pertes\n" %(imax[2], jmax[2], valmax[2]))
+dst.write("\t%s-%s avec %s %% de pertes\n" %(imax[0], jmax[0], valmax[0]))
+dst.write("\t%s-%s avec %s %% de pertes\n" %(imax[1], jmax[1], valmax[1]))
+dst.write("\t%s-%s avec %s %% de pertes\n" %(imax[2], jmax[2], valmax[2]))
 
 src.close()
 dst.close()
