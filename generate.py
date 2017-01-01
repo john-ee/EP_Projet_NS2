f4d62982653a6cc93feb08596a1d2f632df3aa4f
JN <john.nat.hill@gmail.com>
2017-01-01 11:59:35 +0100

    Nouveau fichier de trace basé sur le queue-monitor

--- generate.py
+++ generate.py
@@ -22,11 +22,13 @@ def topologie(src,dst):
 
 		dst.write("$ns duplex-link $n(%s) $n(%s) %sGb %sms DropTail\n" %(topo[0], topo[1], topo[2], topo[3]))
 		dst.write("$ns queue-limit $n(%s) $n(%s) 10\n" %(topo[0],topo[1]))
+		dst.write("set monitor_queue(%s,%s) [$ns monitor-queue $n(%s) $n(%s) queue(%s,%s) 1]\n" %(topo[0], topo[1], topo[0], topo[1], topo[0], topo[1]))
+		dst.write("$ns at 0 \"record %s %s\"\n" %(topo[0], topo[1]))
 		dst.write("\n")
 
 
 def multiple10(x):
-	mult = 100
+	mult = 10
 	cond = True
 	if x < 0:
 		return 0
@@ -58,7 +60,8 @@ def trafic( src, dst, sim_time, burst, idle, shape, nb_flux):
 		ftp_traf = int( data - pareto_traf)
 
 		nb_cycle = (burst + idle) / sim_time
-		rate = int( math.floor( ( pareto_traf / nb_cycle ) / burst ) )
+		#rate = ftp_traf * 2 / sim_time
+		rate = int ( ( pareto_traf / nb_cycle ) / burst ) 
 
 		dst.write("set sink_udp(%s,%s) [new Agent/UDP]\n" %(traf[0], traf[1]))
 		dst.write("$ns attach-agent $n(%s) $sink_udp(%s,%s)\n" %(traf[1], traf[0], traf[1]))
@@ -71,11 +74,9 @@ def trafic( src, dst, sim_time, burst, idle, shape, nb_flux):
 		dst.write("$p(%s,%s) set burst_time_ %s\n" %(traf[0], traf[1], burst))
 		dst.write("$p(%s,%s) set idle_time_ %s\n" %(traf[0], traf[1], idle))
 		dst.write("$p(%s,%s) set shape_ %s\n" %(traf[0], traf[1], shape))
-		dst.write("$p(%s,%s) set rate_ %s G\n" %(traf[0], traf[1], rate))
+		dst.write("$p(%s,%s) set rate_ %s\n" %(traf[0], traf[1], rate))
 		dst.write("$p(%s,%s) attach-agent $udp(%s,%s)\n" %(traf[0], traf[1], traf[0], traf[1]))
-		dst.write("$ns at %s \"$p(%s,%s) start\"\n\n" %(debut, traf[0], traf[1]))
-		dst.write("$ns at %s \"$p(%s,%s) stop\"\n\n" %(fin, traf[0], traf[1]))
-		dst.write("$ns at %s \"$p(%s,%s) start\"\n\n" %(debut, traf[0], traf[1]))
+		dst.write("$ns at %s \"$p(%s,%s) start\"\n" %(debut, traf[0], traf[1]))
 		dst.write("$ns at %s \"$p(%s,%s) stop\"\n\n" %(fin, traf[0], traf[1]))
 
 		random_traf = 0
@@ -89,20 +90,21 @@ def trafic( src, dst, sim_time, burst, idle, shape, nb_flux):
 			sent = zipf.rvs(shape) * offset
 			instant = int(rand.uniform(debut,fin))
 
-			if i > nb_flux-1:
-				dst.write("$ns at %s \"$tcp(%s,%s,%s) send %sG\"\n\n" %(instant, traf[0], traf[1], i%nb_flux, sent))
+			if sent < ftp_traf:
+				if i > nb_flux-1:
+					dst.write("$ns at %s \"$tcp(%s,%s,%s) send %s\"\n\n" %(instant, traf[0], traf[1], i%nb_flux, sent))
 
-			else:
-				dst.write("set tcp(%s,%s,%s) [new Agent/TCP]\n" %(traf[0], traf[1], i))
-				dst.write("$ns attach-agent $n(%s) $tcp(%s,%s,%s)\n" %(traf[0], traf[0], traf[1], i))
+				else:
+					dst.write("set tcp(%s,%s,%s) [new Agent/TCP]\n" %(traf[0], traf[1], i))
+					dst.write("$ns attach-agent $n(%s) $tcp(%s,%s,%s)\n" %(traf[0], traf[0], traf[1], i))
 
-				dst.write("set sink(%s,%s,%s) [new Agent/TCPSink]\n" %(traf[0], traf[1], i))
-				dst.write("$ns attach-agent $n(%s) $sink(%s,%s,%s)\n" %(traf[1], traf[0], traf[1], i))
-				dst.write("$ns connect $tcp(%s,%s,%s) $sink(%s,%s,%s)\n" %(traf[0], traf[1], i, traf[0], traf[1], i))
-				dst.write("$ns at %s \"$tcp(%s,%s,%s) send %sG\"\n\n" %(instant, traf[0], traf[1], i, sent))
+					dst.write("set sink(%s,%s,%s) [new Agent/TCPSink]\n" %(traf[0], traf[1], i))
+					dst.write("$ns attach-agent $n(%s) $sink(%s,%s,%s)\n" %(traf[1], traf[0], traf[1], i))
+					dst.write("$ns connect $tcp(%s,%s,%s) $sink(%s,%s,%s)\n" %(traf[0], traf[1], i, traf[0], traf[1], i))
+					dst.write("$ns at %s \"$tcp(%s,%s,%s) send %s\"\n\n" %(instant, traf[0], traf[1], i, sent))
 
-			random_traf += sent
-			i+=1
+				random_traf += sent
+				i+=1
 
 
 src_topo = open("topo.top","r")
@@ -110,18 +112,32 @@ src_traf = open("traff.traf","r")
 dest = open("simulation.tcl","w")
 
 dest.write("set ns [new Simulator]\n")
-dest.write("set f [open out.tr w]\n$ns trace-all $f\n")
-dest.write("set nf [open out.nam w]\n$ns namtrace-all $nf\n")
+dest.write("set loss_monitor [open loss.tr w]\n\n")
+
 dest.write("proc finish {} {\n")
-dest.write("    global ns f nf\n")
+dest.write("    global ns loss_monitor\n")
 dest.write("    $ns flush-trace\n")
-dest.write("    close $f\n    close $nf\n")
+dest.write("    close $loss_monitor\n")
 dest.write("    exit 0\n")
+dest.write("}\n\n")
+
+dest.write("Node instproc getidnode {} {\n")
+dest.write("	$self instvar id_\n    return \"$id_\"\n}\n\n")
+
+dest.write("proc record {i j} {\n")
+dest.write("	global ns n monitor_queue loss_monitor\n")
+dest.write("	set now [$ns now]\n")
+dest.write("	set next 3\n")
+dest.write("	set from_node [$n($i) getidnode]\n")
+dest.write("	set to_node [$n($j) getidnode]\n")
+dest.write("	$monitor_queue($i,$j) instvar size_ pkts_ barrivals_ bdepartures_ parrivals_ pdepartures_ bdrops_ pdrops_\n")
+dest.write("	puts $loss_monitor \"$now $from_node $to_node $pdrops_ &pkts_\"\n")
+dest.write("	$ns after $next \"record $i $j\"\n")
 dest.write("}\n\n")
 
 topologie(src_topo, dest)
 
-trafic(src_traf, dest, 300, 0.05, 0.05, 2, 41)
+trafic(src_traf, dest, 300, 0.05, 0.05, 1.2, 41)
 
 dest.write("$ns at 300 \"finish\"\n")
 dest.write("puts \"Starting Simulation...\"\n$ns run\n")
--- loss_count.py
+++ loss_count.py
@@ -5,7 +5,7 @@ import numpy as np
 src = open("out.tr","r")
 
 pertes = []
-recus = []
+total = []
 for i in range(25):
 	pertes.append([i] * 25)
 	recus.append([i] * 25)
@@ -13,10 +13,8 @@ for i in range(25):
 for line in src:
 	event = line.rstrip('\n\r').split(" ")
 
-	if event[0] == 'd':
-		pertes[int(event[2])][int(event[3])]+=1
-	elif event[0] == 'r':
-		recus[int(event[2])][int(event[3])]+=1
+	pertes[int(event[1])][int(event[2])]+= int(event[3])
+	total[int(event[1])][int(event[2])]+= int(event[4])
 
 imax = [0]*3
 jmax = [0]*3
@@ -24,16 +22,16 @@ valmax = [0]*3
 
 for i in range(len(pertes)):
 	for j in range(len(pertes[i])):
-		if pertes[i][j] / (recus[i][j]+pertes[i][j]) > valmax[0]:
-			valmax[0] = pertes[i][j] / (recus[i][j]+pertes[i][j])
+		if pertes[i][j] / total[i][j] > valmax[0]:
+			valmax[0] = pertes[i][j] / total[i][j]
 			imax[0] = i
 			jmax[0] = j
-		elif pertes[i][j] / (recus[i][j]+pertes[i][j]) > valmax[1]:
-			valmax[1] = pertes[i][j] / (recus[i][j]+pertes[i][j])
+		elif pertes[i][j] / total[i][j] > valmax[1]:
+			valmax[1] = pertes[i][j] / total[i][j]
 			imax[1] = i
 			jmax[1] = j
-		elif pertes[i][j] / (recus[i][j]+pertes[i][j]) > valmax[2]:
-			valmax[2] = pertes[i][j] / (recus[i][j]+pertes[i][j])
+		elif pertes[i][j] / total[i][j] > valmax[2]:
+			valmax[2] = pertes[i][j] / total[i][j]
 			imax[2] = i
 			jmax[2] = j
 
