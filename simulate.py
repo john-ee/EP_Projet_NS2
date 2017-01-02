0b608b27689e68450564421baeb77ebd841c3e26
JN <john.nat.hill@gmail.com>
2017-01-02 12:13:27 +0100

    Rectification + exception float value

--- simulate.py
+++ simulate.py
@@ -6,5 +6,5 @@ import os
 timer = open("info.txt", "a")
 start = time.time()
 os.system("ns simulation.tcl")
-timer.write("Temps d'execution de la simulation : %sh\n" %(time.time() - start) / 3600)
+timer.write("Temps d'execution de la simulation : %s h\n" %(time.time() - start) / 3600)
 timer.close()
